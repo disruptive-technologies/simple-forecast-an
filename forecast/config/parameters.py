57544d2fe1b7017afa98df8dcde474cee426a93f
jhgjeraker <jhgjeraker@protonmail.com>
2020-08-12 10:51:09 +0200

    added comments to each parameter

--- forecast/config/parameters.py
+++ forecast/config/parameters.py
@@ -1,16 +1,15 @@
 
 # Holt-Winters
-alpha  = 0.02
-beta   = 0.01
-gamma  = 0.75
-theta  = 10
+alpha  = 0.02       # level smoothing factor
+beta   = 0.01       # trend smoothing factor
+gamma  = 0.75       # season smoothing factor
 
 # data related
-season_length  = 7
-n_seasons_init = 7
+season_length  = 110    # number of samples in a season
+n_seasons_init = 7      # number of seasons used in HW initialisation
 
 # forecast
-n_forecast     = season_length*5
-n_step_ahead   = max(1, int(n_forecast/2))
-n_step_ahead   = season_length
-bound_modifier = 1.96
+n_forecast     = season_length*5            # number of samples to forecast
+n_step_ahead   = season_length              # number of samples ahead to use for residual
+bound_modifier = 1.96                       # scaler for bounds
+
