35499d8c362b46bfefdea6b1f2f46b37fc37b8b2
jhgjeraker <jhgjeraker@protonmail.com>
2020-08-20 13:18:45 +0200

    formatting

--- config/parameters.py
+++ config/parameters.py
@@ -1,15 +1,15 @@
 
 # Holt-Winters
-alpha  = 0.02       # level smoothing factor
-beta   = 0.01       # trend smoothing factor
-gamma  = 0.75       # season smoothing factor
+alpha  = 0.02 # level smoothing factor
+beta   = 0.01 # trend smoothing factor
+gamma  = 0.75 # season smoothing factor
 
 # data related
-season_length  = 110    # number of samples in a season
-n_seasons_init = 7      # number of seasons used in HW initialisation
+season_length  = 110 # number of samples in a season
+n_seasons_init = 7   # number of seasons used in HW initialisation
 
 # forecast
-n_forecast     = season_length*4            # number of samples to forecast
-n_step_ahead   = season_length              # number of samples ahead to use for residual
-bound_modifier = 1.96                       # scaler for bounds
+n_forecast     = season_length*4 # number of samples to forecast
+n_step_ahead   = season_length   # number of samples ahead to use for residual
+bound_modifier = 1.96            # scaler for bounds
 
